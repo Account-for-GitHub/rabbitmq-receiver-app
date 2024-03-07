74cea3c01046a4fb1c19df13a6d5cb448de00611
username <email@email.com>
2024-03-07 19:25:54 +0300

    rabbitmq-receiver-app

---
+++ src/main/java/com/example/rabbitmqreceiver/app/Message.java
@@ -0,0 +1,12 @@
+package com.example.rabbitmqreceiver.app;
+
+public class Message {
+    private String text;
+
+    public Message() {
+    }
+
+    public String getText() {
+        return text;
+    }
+}
